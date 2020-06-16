5d098d1a6c5b755319c028075b22fe8dcb342b69
gadi <ngadidasu@gmail.com>
2020-06-15 23:01:05 -0400

    Added the word class

---
+++ app/src/main/java/android/example/miwok/Word.java
@@ -0,0 +1,26 @@
+package android.example.miwok;
+
+/**Word class can be used to create objects which could be used to populate the contents of the items of ListView*/
+public class Word {
+
+    /**private variables for Miwok and Default translation languages*/
+    private String mMiwokTranslation;
+    private String mDefaultTranslation;
+
+    /**constructor method for creating objects of word class*/
+    public Word(String et, String mt){
+        mDefaultTranslation = et;
+        mMiwokTranslation = mt;
+    }
+
+    /**method to access the Miwok translation*/
+    public String getMiwokTranslation(){
+        return mMiwokTranslation;
+    }
+
+    /**method to access the Default translation*/
+    public String getDefaultTranslation(){
+        return mDefaultTranslation;
+    }
+
+}
