897a45e0b9ea8235a9f91cccc271590f38aa1d70
gadi <ngadidasu@gmail.com>
2020-06-15 20:01:27 -0400

    Replace LinearLayout by ListView and ArrayAdapter
    To enable View Recycling and avoid memory leaks

--- app/src/main/java/android/example/miwok/NumbersActivity.java
+++ app/src/main/java/android/example/miwok/NumbersActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -17,8 +19,10 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
 
-        /**String[] words = new String[]{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};*/
+        /** Using the data structure array which has fixed size
+         * String[] words = new String[]{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};*/
 
+        /**Using ArrayList which can have variable size*/
         ArrayList<String> words = new ArrayList<String>();
 
         words.add("one");
@@ -32,17 +36,20 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        LinearLayout rootView = (LinearLayout)  findViewById(R.id.rootView);
+        /** Using linear layout of text views which would be suitable for fixed smaller number of items in a list
+         * LinearLayout rootView = (LinearLayout)  findViewById(R.id.rootView);
+        *byte i = 0;
+        *while(i<words.size()) {
+        *    TextView textView = new TextView(this);
+        *    textView.setText(words.get(i));
+        *    rootView.addView(textView);
+        *    i++;
+        }*/
 
-        byte i = 0;
-
-        while(i<words.size()) {
-            TextView textView = new TextView(this);
-            textView.setText(words.get(i));
-            rootView.addView(textView);
-            i++;
-        }
-
+        /** Using ListView with array adapter will allow view recycling for large number of views preventing memory leak issues*/
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
     }
 
 }
