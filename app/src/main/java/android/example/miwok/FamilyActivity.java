cf302a79707415c9621f0ae47dba3f7fef868bb0
gadi <ngadidasu@gmail.com>
2020-06-16 22:01:04 -0400

    Replicated code in all activities

--- app/src/main/java/android/example/miwok/FamilyActivity.java
+++ app/src/main/java/android/example/miwok/FamilyActivity.java
@@ -3,12 +3,62 @@ package android.example.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.activity_numbers);
+
+
+        /** Using the data structure array which has fixed size
+         * String[] words = new String[]{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};*/
+
+        /**Using ArrayList which can have variable size*/
+        ArrayList<Word> words = new ArrayList<Word>();
+
+        words.add(new Word("one", "lutti"));
+        words.add(new Word("two", "otiiko"));
+        words.add(new Word("three", "tolookosu"));
+        words.add(new Word("four", "oyyisa"));
+        words.add(new Word("five", "massokka"));
+        words.add(new Word("six", "temmokka"));
+        words.add(new Word("seven", "kenekaku"));
+        words.add(new Word("eight", "kawinta"));
+        words.add(new Word("nine","wo'e"));
+        words.add(new Word("ten","na'acha"));
+
+        /** Using linear layout of text views which would be suitable for fixed smaller number of items in a list
+         * LinearLayout rootView = (LinearLayout)  findViewById(R.id.rootView);
+         *byte i = 0;
+         *while(i<words.size()) {
+         *    TextView textView = new TextView(this);
+         *    textView.setText(words.get(i));
+         *    rootView.addView(textView);
+         *    i++;
+         }*/
+
+        /** Using ListView with array adapter will allow view
+         * recycling for large number of views preventing memory
+         * leak issues*/
+        /**ArrayAdapter<Word> itemsAdapter = new ArrayAdapter<Word>(this, R.layout.list_item, words);*/
+
+        /**Using custom defined word adapter to handle the cases
+         * where we need to fill in multiple views within each
+         *list item
+         */
+        WordAdapter itemsAdapter = new WordAdapter(this, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
     }
+
 }
