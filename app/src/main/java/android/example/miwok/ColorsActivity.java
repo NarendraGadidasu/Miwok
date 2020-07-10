ef2ffd0f5dfdbb20a7f48cddf79b2dc91420ab46
gadi <ngadidasu@gmail.com>
2020-07-09 21:29:04 -0400

    added code to abandon audio focus

--- app/src/main/java/android/example/miwok/ColorsActivity.java
+++ app/src/main/java/android/example/miwok/ColorsActivity.java
@@ -138,6 +138,8 @@ public class ColorsActivity extends AppCompatActivity {
             // setting the media player to null is an easy way to tell that the media player
             // is not configured to play an audio file at the moment.
             mediaPlayer = null;
+
+            audioManager.abandonAudioFocus(afChangeListener);
         }
     }
 
--- app/src/main/java/android/example/miwok/FamilyActivity.java
+++ app/src/main/java/android/example/miwok/FamilyActivity.java
@@ -144,6 +144,8 @@ public class FamilyActivity extends AppCompatActivity {
             // setting the media player to null is an easy way to tell that the media player
             // is not configured to play an audio file at the moment.
             mediaPlayer = null;
+
+            audioManager.abandonAudioFocus(afChangeListener);
         }
     }
 
